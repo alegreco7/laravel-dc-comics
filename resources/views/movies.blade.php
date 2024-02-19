b151a2f4b17ac1ea8545b0ab13b21fd44a4d8afb
alegreco7 <alexgreco2002@gmail.com>
2024-02-19 12:38:02 +0100

    movies section empty

---
+++ resources/views/movies.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.base')
+
+
+@section('content')
+    <div class="jumbo-container">
+        <span class="default-badge-bg">CURRENT SERIES</span>
+    </div>
+    <div>
+        <div class="container py-5">
+            <div class="col-12 py-5">
+            </div>
+        </div>
+    </div>
+@endsection
